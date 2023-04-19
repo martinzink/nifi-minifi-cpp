4339750bed65532aa5fce79500d14653c6f95b9c
Martin Zink <martinzink@apache.org>
2023-04-19 10:19:30 +0200

    some

--- libminifi/src/utils/file/FileUtils.cpp
+++ libminifi/src/utils/file/FileUtils.cpp
@@ -68,10 +68,8 @@ bool contains(const std::filesystem::path& file_path, std::string_view text_to_s
 
 std::chrono::system_clock::time_point to_sys(std::filesystem::file_time_type file_time) {
   using namespace std::chrono;  // NOLINT(build/namespaces)
-#if defined(WIN32)
-  return clock_cast<system_clock>(file_time);
-#elif defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION < 14000)
-  return system_clock::from_time_t(file_clock::to_time_t(file_time));
+#if defined(WIN32) || (defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION < 14000))
+  return system_clock::now() + (file_time - file_clock::now());
 #else
   return time_point_cast<system_clock::duration>(file_clock::to_sys(file_time));
 #endif
@@ -79,10 +77,8 @@ std::chrono::system_clock::time_point to_sys(std::filesystem::file_time_type fil
 
 std::filesystem::file_time_type from_sys(std::chrono::system_clock::time_point sys_time) {
   using namespace std::chrono;  // NOLINT(build/namespaces)
-#if defined(WIN32)
-  return clock_cast<file_clock>(sys_time);
-#elif defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION < 14000)
-  return file_clock::from_time_t(system_clock::to_time_t(sys_time));
+#if defined(WIN32) || (defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION < 14000))
+  return file_clock::now() + (sys_time - system_clock::now());
 #else
   return time_point_cast<file_clock::duration>(file_clock::from_sys(sys_time));
 #endif
