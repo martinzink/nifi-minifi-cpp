2cde83da6e5fda90bd3956c190639ee3ff32be5d
Martin Zink <martinzink@apache.org>
2023-07-10 15:41:41 +0200

    Add heterogeneous lookup to FlatMap

--- extensions/standard-processors/processors/TextFragmentUtils.h
+++ extensions/standard-processors/processors/TextFragmentUtils.h
@@ -21,9 +21,9 @@
 #include "utils/Export.h"
 
 namespace org::apache::nifi::minifi::processors::textfragmentutils {
-  constexpr const char* BASE_NAME_ATTRIBUTE = "TextFragmentAttribute.base_name";
-  constexpr const char* POST_NAME_ATTRIBUTE = "TextFragmentAttribute.post_name";
-  constexpr const char* OFFSET_ATTRIBUTE = "TextFragmentAttribute.offset";
+  constexpr std::string_view BASE_NAME_ATTRIBUTE = "TextFragmentAttribute.base_name";
+  constexpr std::string_view POST_NAME_ATTRIBUTE = "TextFragmentAttribute.post_name";
+  constexpr std::string_view OFFSET_ATTRIBUTE = "TextFragmentAttribute.offset";
 
   inline std::string createFileName(const std::string& base_name, const std::string& post_name, const size_t offset, const size_t size) {
     return base_name + "." + std::to_string(offset) + "-" + std::to_string(offset + size - 1) + "." + post_name;
