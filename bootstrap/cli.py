4814f556faa1fcafffdbb39a3f580c635c9c06ef
Martin Zink <martinzink@apache.org>
2023-11-10 14:19:18 +0100

    anytime

--- bootstrap/cli.py
+++ bootstrap/cli.py
@@ -25,18 +25,19 @@ from system_dependency import install_required
 
 
 def install_dependencies(minifi_options: MinifiOptions, package_manager: PackageManager):
+    print("install_dependencies")
     install_required(minifi_options, package_manager)
 
 
 def run_cmake(minifi_options: MinifiOptions, _package_manager: PackageManager):
+    print("run_cmake")
     if not os.path.exists(minifi_options.build_dir):
         os.mkdir(minifi_options.build_dir)
-    os.chdir(minifi_options.build_dir)
-    cmake_cmd = f"cmake -G Ninja {minifi_options.create_cmake_options_str()} {minifi_options.source_dir}"
+    cmake_cmd = f"cmake -G Ninja {minifi_options.create_cmake_options_str()} {minifi_options.source_dir} -B {minifi_options.build_dir}"
     try:
         subprocess.run(cmake_cmd, shell=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Build failed with exit code {e.returncode}")
+        print(f"Cmake failed with exit code {e.returncode}")
         print("Standard Output:")
         print(e.stdout)
         print("Standard Error:")
@@ -45,6 +46,7 @@ def run_cmake(minifi_options: MinifiOptions, _package_manager: PackageManager):
 
 
 def do_build(minifi_options: MinifiOptions, _package_manager: PackageManager):
+    print("do_build")
     try:
         subprocess.run(['cmake', '--build', minifi_options.build_dir], text=True, check=True)
     except subprocess.CalledProcessError as e:
