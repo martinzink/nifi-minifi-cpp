8163b1c9865c163c2cfbbaf8d93010bd22162917
Martin Zink <martinzink@apache.org>
2023-11-28 17:20:44 +0100

    kaboom

--- bootstrap/package_manager.py
+++ bootstrap/package_manager.py
@@ -290,6 +290,7 @@ class ChocolateyPackageManager(PackageManager):
         return ""
 
     def run_cmd(self, cmd: str) -> bool:
+        print(f"{cmd}".center(60, '*'))
         res = subprocess.run(f"refreshenv & {_get_vsdev_cmd()} & {cmd}", shell=True)
 
         return res.returncode == 0
