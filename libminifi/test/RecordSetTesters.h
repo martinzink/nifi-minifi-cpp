167c91cf55d64284c2be71656837a084d8dc30b1
Martin Zink <martinzink@apache.org>
2024-04-16 09:38:41 +0200

    removed gcc11 workaround

--- libminifi/test/RecordSetTesters.h
+++ libminifi/test/RecordSetTesters.h
@@ -60,7 +60,7 @@ bool testRecordWriter(RecordSetWriter& record_set_writer, const RecordSet& recor
   return tester(flow_file_content);
 }
 
-inline bool testRecordReader(RecordSetReader& record_set_reader, const std::string_view serialized_record_set, const RecordSet& expected_record_set) {
+inline void testRecordReader(RecordSetReader& record_set_reader, const std::string_view serialized_record_set, const RecordSet& expected_record_set) {
   const RecordSetFixture fixture;
   ProcessSession& process_session = fixture.processSession();
 
@@ -70,13 +70,8 @@ inline bool testRecordReader(RecordSetReader& record_set_reader, const std::stri
   process_session.commit();
 
   const auto record_set = record_set_reader.read(flow_file, process_session);
-  if (!record_set)
-    return false;
-  auto& record_set_0 = (*record_set)[0];
-  auto& expected_set_0 = expected_record_set[0];
-  CHECK(record_set_0 == expected_set_0);
-
-  return *record_set == expected_record_set;
+  REQUIRE(record_set);
+  CHECK(*record_set == expected_record_set);
 }
 
 }  // namespace org::apache::nifi::minifi::core::test
