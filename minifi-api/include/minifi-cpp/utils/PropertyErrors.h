458b1487862a83d1cffbe4c40aec389c4f462502
Adam Debreceni <adebreceni@apache.org>
2024-09-16 16:44:17 +0200

    Separate out minifi-api/extension-utils/utils from libminifi

--- minifi-api/include/minifi-cpp/utils/PropertyErrors.h
+++ minifi-api/include/minifi-cpp/utils/PropertyErrors.h
@@ -1,5 +1,5 @@
 /**
- *
+*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +7,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenseas/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,72 +19,45 @@
 #pragma once
 
 #include <string>
+#include <system_error>
+#include "magic_enum.hpp"
 
-#include "Exception.h"
+namespace org::apache::nifi::minifi::core {
 
-namespace org {
-namespace apache {
-namespace nifi {
-namespace minifi {
-
-namespace core {
-
-class PropertyValue;
-class ConfigurableComponent;
-class Property;
-
-} /* namespace core */
-
-namespace utils {
-namespace internal {
-
-class ValueException : public Exception {
- public:
-  explicit ValueException(const std::string& err) : Exception(ExceptionType::GENERAL_EXCEPTION, err) {}
-  explicit ValueException(const char* err) : Exception(ExceptionType::GENERAL_EXCEPTION, err) {}
-
-  // base class already has a virtual destructor
+enum class PropertyErrorCode : std::underlying_type_t<std::byte> {
+  NotSupportedProperty,
+  DynamicPropertiesNotSupported,
+  PropertyNotSet,
+  ValidationFailed,
+  EmptyString
 };
 
-class PropertyException : public Exception {
- public:
-  explicit PropertyException(const std::string& err) : Exception(ExceptionType::GENERAL_EXCEPTION, err) {}
-  explicit PropertyException(const char* err) : Exception(ExceptionType::GENERAL_EXCEPTION, err) {}
 
-  // base class already has a virtual destructor
+struct PropertyErrorCategory final : std::error_category {
+  [[nodiscard]] const char* name() const noexcept override {
+    return "property error";
+  }
+
+  [[nodiscard]] std::string message(int ev) const override {
+    const auto ec = static_cast<PropertyErrorCode>(ev);
+    auto e_str = std::string{magic_enum::enum_name<PropertyErrorCode>(ec)};
+    if (e_str.empty()) {
+      return "UNKNOWN ERROR";
+    }
+    return e_str;
+  }
 };
 
-/**
- * Thrown during converting from and to Value
- */
-class ConversionException : public ValueException {
-  using ValueException::ValueException;
+inline const PropertyErrorCategory& property_error_category() noexcept {
+  static PropertyErrorCategory category;
+  return category;
 };
 
-/**
- * Represents std::string -> Value conversion errors
- */
-class ParseException : public ConversionException {
-  using ConversionException::ConversionException;
-};
+inline std::error_code make_error_code(PropertyErrorCode c) {
+  return {static_cast<int>(c), property_error_category()};
+}
 
-/**
- * Thrown when trying to access invalid Values.
- */
-class InvalidValueException : public ValueException {
-  using ValueException::ValueException;
-};
+}  // namespace org::apache::nifi::minifi::core
 
-/**
- * When querying missing properties marked required.
- */
-class RequiredPropertyMissingException : public PropertyException {
-  using PropertyException::PropertyException;
-};
-
-} /* namespace internal */
-} /* namespace utils */
-} /* namespace minifi */
-} /* namespace nifi */
-} /* namespace apache */
-} /* namespace org */
+template <>
+struct std::is_error_code_enum<org::apache::nifi::minifi::core::PropertyErrorCode> : std::true_type {};
