f7f89dc6d0ecfd7847b1618e689f55f7967ae257
Martin Zink <martinzink@apache.org>
2025-02-21 14:08:38 +0100

    MINIFICPP-2542 ConsumeKafka late offset commit

--- utils/include/utils/expected.h
+++ utils/include/utils/expected.h
@@ -23,6 +23,7 @@
 #include "nonstd/expected.hpp"
 #include "utils/detail/MonadicOperationWrappers.h"
 #include "fmt/format.h"
+#include "fmt/std.h"
 #include "utils/Error.h"  // for more readable std::error_code fmt::formatter
 
 namespace org::apache::nifi::minifi::utils {
@@ -229,3 +230,34 @@ auto try_expression(F&& action, Args&&... args) noexcept {
 }
 
 }  // namespace org::apache::nifi::minifi::utils
+
+#ifndef WIN32
+// based on fmt::formatter<std::expected<T, E>, Char
+template <typename T, typename E, typename Char>
+struct fmt::formatter<nonstd::expected<T, E>, Char,
+                 std::enable_if_t<(std::is_void<T>::value ||
+                                   fmt::is_formattable<T, Char>::value) &&
+                                  fmt::is_formattable<E, Char>::value>> {
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) -> const Char* {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const nonstd::expected<T, E>& value, FormatContext& ctx) const
+      -> decltype(ctx.out()) {
+    auto out = ctx.out();
+
+    if (value.has_value()) {
+      out = fmt::detail::write<Char>(out, "expected(");
+      if constexpr (!std::is_void<T>::value)
+        out = fmt::detail::write_escaped_alternative<Char>(out, *value);
+    } else {
+      out = fmt::detail::write<Char>(out, "unexpected(");
+      out = fmt::detail::write_escaped_alternative<Char>(out, value.error());
+    }
+    *out++ = ')';
+    return out;
+  }
+};
+#endif
