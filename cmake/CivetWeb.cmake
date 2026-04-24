3e242f49fef54aafa8345d5c71ef9eaff157ed2a
Martin Zink <martinzink@apache.org>
2026-04-24 10:43:56 +0200

    openssl streamlined 5

--- cmake/CivetWeb.cmake
+++ cmake/CivetWeb.cmake
@@ -41,8 +41,8 @@ FetchContent_Declare(civetweb
 
 FetchContent_MakeAvailable(civetweb)
 
-target_link_libraries(civetweb-c-library PUBLIC OpenSSL::SSL OpenSSL::Crypto)
-target_link_libraries(civetweb-cpp PUBLIC OpenSSL::SSL OpenSSL::Crypto)
+target_link_libraries(civetweb-c-library OpenSSL::SSL OpenSSL::Crypto)
+target_link_libraries(civetweb-cpp OpenSSL::SSL OpenSSL::Crypto)
 
 target_compile_definitions(civetweb-c-library PRIVATE SOCKET_TIMEOUT_QUANTUM=200)
 
