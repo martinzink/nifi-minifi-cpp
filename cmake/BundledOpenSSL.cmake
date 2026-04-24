8169613b5f47b39341fef5e579ffaea193fa8aff
Martin Zink <martinzink@apache.org>
2026-04-24 14:18:45 +0200

    openssl streamlined 12

--- cmake/BundledOpenSSL.cmake
+++ cmake/BundledOpenSSL.cmake
@@ -18,14 +18,12 @@
 function(use_openssl SOURCE_DIR BINARY_DIR)
     message(STATUS "Using bundled OpenSSL")
 
-    # 1. Platform-specific library directory
     if (APPLE OR WIN32 OR CMAKE_SIZEOF_VOID_P EQUAL 4 OR CMAKE_SYSTEM_PROCESSOR MATCHES "^(arm64|aarch64|armv8)$")
         set(LIBDIR "lib")
     else ()
         set(LIBDIR "lib64")
     endif ()
 
-    # 2. Native CMake library extensions
     set(OPENSSL_BUILD_SHARED OFF)
     set(LIB_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
     set(LIB_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
@@ -50,7 +48,6 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
     set(OPENSSL_EXTRA_FLAGS no-tests no-capieng no-docs no-legacy enable-tfo no-ssl no-engine)
     set(OPENSSL_VERSION "3.3.6")
 
-    # 3. Build Commands
     if (WIN32)
         find_program(JOM_EXECUTABLE NAMES jom.exe PATHS ENV PATH NO_DEFAULT_PATH)
         if (JOM_EXECUTABLE)
@@ -75,14 +72,13 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
         set(OSSL_INSTALL make install)
     endif ()
 
-    # 4. Main OpenSSL ExternalProject
     ExternalProject_Add(
             openssl-external
             URL "https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERSION}/openssl-${OPENSSL_VERSION}.tar.gz"
             URL_HASH "SHA256=22db04f3c8f9a808c9795dcf7d2713ff40c12c410ea2d1f6435c6c9c8558958b"
             SOURCE_DIR "${BINARY_DIR}/thirdparty/openssl-src"
             BUILD_IN_SOURCE TRUE
-            CONFIGURE_COMMAND ${OSSL_CONFIGURE} ${OPENSSL_SHARED_FLAG} ${OPENSSL_EXTRA_FLAGS} "--prefix=${OPENSSL_BIN_DIR}" "--openssldir=${OPENSSL_BIN_DIR}"
+            CONFIGURE_COMMAND ${OSSL_CONFIGURE} ${OPENSSL_SHARED_FLAG} ${OPENSSL_EXTRA_FLAGS} "--prefix=${OPENSSL_BIN_DIR}" "--openssldir=${OPENSSL_BIN_DIR}" "--libdir=${LIBDIR}"
             BUILD_BYPRODUCTS "${OPENSSL_CRYPTO_LIB}" "${OPENSSL_SSL_LIB}"
             EXCLUDE_FROM_ALL TRUE
             BUILD_COMMAND ${OPENSSL_BUILD_COMMAND}
@@ -91,7 +87,6 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
             TLS_VERIFY TRUE
     )
 
-    # 5. Create GLOBAL Imported Targets
     file(MAKE_DIRECTORY "${OPENSSL_BIN_DIR}/include")
 
     add_library(OpenSSL::Crypto UNKNOWN IMPORTED GLOBAL)
@@ -115,9 +110,8 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
         set_property(TARGET OpenSSL::SSL APPEND PROPERTY INTERFACE_LINK_LIBRARIES crypt32.lib)
     endif ()
 
-    # 6. FIPS Build - Streamlined
-    set(FIPS_MODULE "${OPENSSL_BIN_DIR}/thirdparty/openssl-fips-install/${LIBDIR}/ossl-modules/fips${CMAKE_SHARED_LIBRARY_SUFFIX}")
     set(OPENSSL_FIPS_BIN_DIR "${BINARY_DIR}/thirdparty/openssl-fips-install")
+    set(FIPS_MODULE "${OPENSSL_FIPS_BIN_DIR}/${LIBDIR}/ossl-modules/fips${CMAKE_SHARED_LIBRARY_SUFFIX}")
     set(OPENSSL_FIPS_EXTRA_FLAGS no-tests no-capieng no-legacy no-ssl no-engine enable-fips)
 
     if (WIN32)
@@ -132,7 +126,7 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
             URL_HASH "SHA256=eb1ab04781474360f77c318ab89d8c5a03abc38e63d65a603cabbf1b00a1dc90"
             SOURCE_DIR "${BINARY_DIR}/thirdparty/openssl-fips-src"
             BUILD_IN_SOURCE TRUE
-            CONFIGURE_COMMAND ${OSSL_CONFIGURE} ${OPENSSL_SHARED_FLAG} ${OPENSSL_FIPS_EXTRA_FLAGS} "--prefix=${OPENSSL_FIPS_BIN_DIR}" "--openssldir=${OPENSSL_FIPS_BIN_DIR}"
+            CONFIGURE_COMMAND ${OSSL_CONFIGURE} ${OPENSSL_SHARED_FLAG} ${OPENSSL_FIPS_EXTRA_FLAGS} "--prefix=${OPENSSL_FIPS_BIN_DIR}" "--openssldir=${OPENSSL_FIPS_BIN_DIR}" "--libdir=${LIBDIR}"
             BUILD_BYPRODUCTS "${FIPS_MODULE}"
             EXCLUDE_FROM_ALL TRUE
             BUILD_COMMAND ${OPENSSL_BUILD_COMMAND}
@@ -140,7 +134,6 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
     )
     add_dependencies(OpenSSL::Crypto openssl-fips-external)
 
-    # 7. Packaging setup
     if (MINIFI_PACKAGING_TYPE MATCHES "^(RPM|TGZ)$")
         set(FIPS_DEST "${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}/fips")
         if (MINIFI_PACKAGING_TYPE STREQUAL "TGZ")
@@ -153,7 +146,6 @@ function(use_openssl SOURCE_DIR BINARY_DIR)
                 PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ WORLD_READ WORLD_EXECUTE)
     endif ()
 
-    # 8. EXPORT ONLY THE ROOT DIR TO PARENT SCOPE
     # This is the single source of truth for the strict FindOpenSSL.cmake
     set(OPENSSL_ROOT_DIR "${OPENSSL_BIN_DIR}" CACHE INTERNAL "Strict single source of truth for bundled OpenSSL")
 endfunction(use_openssl)
